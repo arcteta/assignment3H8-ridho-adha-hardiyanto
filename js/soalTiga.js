033dce99b20618de7e6eb285ee8183b42d6da4e0
ridho hardi <ridho@bithealth.co.id>
2024-01-16 11:58:49 +0700

    init soalTiga.js

---
+++ js/soalTiga.js
@@ -0,0 +1,12 @@
+const countSumArr = (arr) => {
+
+    const sortedArr = arr.sort((a, b) => { return a - b })
+
+    return ;
+
+}
+
+const testCase = [2, 1, 4, 3];
+
+
+console.log(countSumArr(testCase));
