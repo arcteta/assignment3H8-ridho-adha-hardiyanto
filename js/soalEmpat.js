dc0ded67af35be0b8c44a2fba282917ff7632f5b
ridho hardi <ridho@bithealth.co.id>
2024-01-16 15:56:47 +0700

    [update]soal no 4 unfinished

---
+++ js/soalEmpat.js
@@ -0,0 +1,19 @@
+const findHighestCountArr = (arr, arrLength) => {
+
+    let maxInt = Math.pow(2, 53);
+    let maxSoFar = maxInt - 1;
+    let maxEndingHere = 0;
+
+    for (let i = 0; i < test.length; i++) {
+
+        maxEndingHere = maxEndingHere + arr[i]
+        if(maxSoFar < max)
+    }
+
+    return
+
+}
+
+arr = [-2, -3, 4, -1, -2, 1, 5, -3]
+
+console.log(findHighestCountArr(arr))
